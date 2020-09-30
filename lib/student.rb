75bb9a32d3507824df90f943c50c529a6922e034
Sara A <calisaraalhindi@gmail.com>
2020-09-30 18:26:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
-  def self.create 
+  def self.create(name, album) 
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
