e905a732b1b4101efc867342773f56dbeea7ff2d
Sara A <calisaraalhindi@gmail.com>
2020-09-30 18:10:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,7 +28,17 @@ class Student
   end
   
   def save 
-    
+     def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
+ 
  
  
   
