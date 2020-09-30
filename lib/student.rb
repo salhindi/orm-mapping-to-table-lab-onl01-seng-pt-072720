557c89ed480497f59d2f8e2ad8dc6aad67f83b5c
Sara A <calisaraalhindi@gmail.com>
2020-09-30 18:11:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,8 @@ class Student
   
   def save 
      sql =  <<-SQL 
-     INSERT INTO songs (name, grade) 
+     INSERT INTO songs (name, grade)
+     VALUES (?,?)
         SQL
     DB[:conn].execute(sql) 
   end
