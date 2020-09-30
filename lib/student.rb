22ce479f3b6a44908256516722a10e85cca75601
Sara A <calisaraalhindi@gmail.com>
2020-09-30 18:27:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
   end
   
   def self.create(name, grade)
-    student = 
+    student = Student.new
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
