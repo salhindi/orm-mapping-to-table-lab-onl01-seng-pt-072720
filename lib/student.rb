35e52075a4f4e8260f93d476e76d1a23bf2bae55
Sara A <calisaraalhindi@gmail.com>
2020-09-30 17:54:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,11 @@
 class Student
+  attr_accessor :name, :grade
+  attr_writer :id
+  
+  def intialize(name, grade)
+    @name = name
+    @grade = grade
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
