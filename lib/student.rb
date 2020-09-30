3047b0f585b10af20f0a002c1ac3d67355e6171c
Sara A <calisaraalhindi@gmail.com>
2020-09-30 18:00:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-  def intialize(name, grade)
+  def intialize(name, grade, id)
     @name = name
     @grade = grade
     @id = id
