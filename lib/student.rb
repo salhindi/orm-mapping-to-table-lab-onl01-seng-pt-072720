137ec742354dfff9ff34105afd4945d55142b2e3
Sara A <calisaraalhindi@gmail.com>
2020-09-30 18:27:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,6 +41,7 @@ class Student
     student = Student.new 
     student.save 
     student 
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
