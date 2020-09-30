c243958d7882698d2a9c5e836e2ed5389dacff23
Sara A <calisaraalhindi@gmail.com>
2020-09-30 18:17:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,10 +34,6 @@ class Student
         SQL
     DB[:conn].execute(sql, self.name, self.grade) 
   end
- 
- 
- 
-  
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
